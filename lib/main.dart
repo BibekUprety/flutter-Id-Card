e2304a6e8d33a36501f385b4d8d79879e799bb6c
Bibek Upreti <bibekupreti9813@gmail.com>
2020-10-07 14:22:14 +0545

    flitter id card

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: BibekCard(),
+    ));
+
+class BibekCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("Bibek ID Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+              backgroundImage:   AssetImage("assets/bibek.jpg"),
+                radius: 50,
+              ),
+            ),
+            Divider(
+              height: 40,
+              color: Colors.grey[800],
+            ),
+            Text(
+              "NAME",
+              style: TextStyle(
+                letterSpacing: 2,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "BIBEK",
+              style: TextStyle(
+                  letterSpacing: 2,
+                  color: Colors.amberAccent[200],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "CURREBT BIBEK LEVEL",
+              style: TextStyle(
+                letterSpacing: 2,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "8",
+              style: TextStyle(
+                  letterSpacing: 2,
+                  color: Colors.amberAccent[200],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text("bibek@gmail.com",
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 2.0,
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
